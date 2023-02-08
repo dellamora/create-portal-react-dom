c5a844eca90401c89625ec71bd401b3ec892bb3c
Dellamora <francielle@dellamora.dev>
2023-02-08 18:35:20 -0300

    added modal component at home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import { type NextPage } from "next";
-import Head from "next/head";
-import Link from "next/link";
+import { useState } from "react";
+import Modal from "../components/modal";
 
 const Home: NextPage = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b to-[#131313] from-[#1E1E1E]">
@@ -10,8 +11,17 @@ const Home: NextPage = () => {
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
           Desvendando o <span className="text-[#F83941]">createPortal</span>   do React
           </h1>
-          <button className="font-semibold py-[10px] px-[20px] border rounded border-[#F83941] transition-colors hover:bg-[#f8394355] text-white ">Modal</button>
+          <button 
+           onClick={() => setIsOpen(true)}
+           className="font-semibold py-[10px] px-[20px] border rounded border-[#F83941] transition-colors hover:bg-[#f8394355] text-white "
+          >
+            Modal
+          </button>
         </div>
+        <Modal
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+      />
       </main>
     </>
   );
